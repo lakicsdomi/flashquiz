83d9b48b918819841acf58b582d4a9243fbaaace
Domonkos Lakics <qq7oak@INF.ELTE.HU>
2025-05-20 11:10:41 +0200

    Added `Quiz`, which manages the quizzing part.

---
+++ FlashQuiz/FlashQuiz/Quiz.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashQuiz
+{
+    public class Quiz
+    {
+        public string QuizName { get; set; }
+        private int _QuestionCount;
+        private int _CorrectAnswers;
+        
+        public List<Question> Questions { get; set; }
+        public Quiz(string quizName, List<Question> questions)
+        {
+            QuizName = quizName;
+            Questions = questions;
+        }
+        public void DisplayQuiz()
+        {
+            Console.WriteLine($"Quiz: {QuizName}");
+            foreach (var question in Questions)
+            {
+                Console.WriteLine($"Score: {_CorrectAnswers}/{_QuestionCount}");
+                Console.WriteLine(question.QuestionText);
+                foreach (var answer in question.Answers)
+                {
+                    Console.WriteLine($"{answer.Item1}: {answer.Item2}");
+                }
+                Console.Write("Please select an answer: ");
+                string userAnswer = Console.ReadLine() ?? string.Empty;
+                if(userAnswer.Equals(question.CorrectAnswer.Item1, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Correct!");
+                    _CorrectAnswers++;
+                }
+                else
+                {
+                    Console.WriteLine($"Incorrect! The correct answer is: {question.CorrectAnswer.Item2}");
+                }
+                _QuestionCount++;
+            }
+        }
+    }
+}
